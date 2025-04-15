93f3e3217ad48ce312c7262b3d32301933bbad1f
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2025-04-15 23:50:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Consult from "./pages/doctor/Consult";
+import Medications from "./pages/doctor/Medications";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -57,6 +59,8 @@ const App = () => (
             <Route path="/doctor/patients" element={<PatientManagement />} />
             <Route path="/doctor/profile" element={<DoctorProfile />} />
             <Route path="/notifications" element={<NotificationCenter />} />
+            <Route path="/doctor/consult" element={<Consult />} />
+            <Route path="/doctor/medications" element={<Medications />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
