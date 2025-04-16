d43c6c10c86f569855deaa2aed512e05c9dcab96
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2025-04-16 09:32:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import Consult from "./pages/doctor/Consult";
-import Medications from "./pages/doctor/Medications";
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
