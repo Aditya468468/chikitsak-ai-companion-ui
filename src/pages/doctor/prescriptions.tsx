6c9e151f14e4435fadc8f1d975cbdfcd0b81d3cb
Aditya Singh <160159510+Aditya468468@users.noreply.github.com>
2025-04-16 09:28:38 +0530

    Create prescriptions.tsx

---
+++ src/pages/doctor/prescriptions.tsx
@@ -0,0 +1,8 @@
+export default function PrescriptionsPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Past Prescriptions</h1>
+      <p>This is where you’ll see prescription data.</p>
+    </div>
+  )
+}
